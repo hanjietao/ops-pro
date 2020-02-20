490e6ef2f2b554260968b0cfe12f53eafef555be
hanjietao <hjtxyr@163.com>
2020-02-20 11:47:00 +0800

    医疗项目新增医院id

--- sql/bussiness.sql
+++ sql/bussiness.sql
@@ -109,6 +109,7 @@ CREATE TABLE ch_medical_project(
     NAME VARCHAR(64)    COMMENT '医疗项目名称' ,
     SERVICE_CONTENT VARCHAR(1024)    COMMENT '服务内容' ,
     PRICE DECIMAL(32,8)    COMMENT '收费标准 45' ,
+    HOSPITAL_ID INT(11)    COMMENT '医院id' ,
     DELETE_FLAG VARCHAR(1)  DEFAULT '0'  COMMENT '删除标志 1-已删除，0-未删除' ,
     STATUS CHAR(1)    COMMENT '状态 0-正常，1-关闭' ,
     CREATE_BY VARCHAR(32)    COMMENT '创建人' ,
