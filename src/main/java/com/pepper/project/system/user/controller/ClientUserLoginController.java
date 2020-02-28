a78a9f93f1c7c2f41923ae40d545779370520f46
hanjietao <hjtxyr@163.com>
2020-02-28 19:23:31 +0800

    组织架构改造

--- src/main/java/com/pepper/project/system/user/controller/ClientUserLoginController.java
+++ src/main/java/com/pepper/project/system/user/controller/ClientUserLoginController.java
@@ -1,19 +1,10 @@
 package com.pepper.project.system.user.controller;
 
-import com.pepper.common.utils.ServletUtils;
 import com.pepper.common.utils.StringUtils;
-import com.pepper.framework.aspectj.lang.enums.SysUserType;
 import com.pepper.framework.web.controller.BaseController;
 import com.pepper.framework.web.domain.AjaxResult;
-import com.pepper.project.ch.hospital.domain.Hospital;
-import com.pepper.project.ch.hospital.service.IHospitalService;
-import com.pepper.project.cm.community.domain.Community;
-import com.pepper.project.cm.community.service.ICommunityService;
-import com.pepper.project.pm.property.domain.Property;
-import com.pepper.project.pm.property.service.IPropertyService;
 import com.pepper.project.sm.user.domain.ClientUser;
 import com.pepper.project.sm.user.service.IClientUserService;
-import com.pepper.project.system.user.domain.Merchant;
 import com.pepper.project.system.user.domain.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
@@ -21,12 +12,8 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * 登录验证
